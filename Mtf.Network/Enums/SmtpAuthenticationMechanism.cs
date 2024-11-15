ba6fcd891046fef1a05abc5d365b24a5614cb9bd
Mortens <mortens.4444@gmail.com>
2024-11-15 17:07:12 +0100

    Fix missing SmtpAuthenticationMechanism

---
+++ Mtf.Network/Enums/SmtpAuthenticationMechanism.cs
@@ -0,0 +1,11 @@
+﻿namespace Mtf.Network.Enums
+{
+    public enum SmtpAuthenticationMechanism
+    {
+        Plain,
+        Login,
+        CramMd5,
+        DigestMd5,
+        XoAuth2
+    }
+}
--- Mtf.Network/SmtpClient.cs
+++ Mtf.Network/SmtpClient.cs
@@ -1,7 +1,6 @@
-﻿using Mtf.Network.Enums;
+﻿using System;
 using System.Net.Sockets;
 using System.Text;
-using System;
 
 namespace Mtf.Network
 {
