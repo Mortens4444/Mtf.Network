49501793ea30779d397dc85510fde391ef14dc17
Mortens <mortens.4444@gmail.com>
2025-01-16 18:51:53 +0100

    Add WakeOnLAN

---
+++ Mtf.Network/Services/MacAddressConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Mtf.Network.Services
+{
+    public static class MacAddressConverter
+    {
+        public const int MacAddressLengthInBytes = 6;
+
+        public static byte[] StringToByteArray(string macString)
+        {
+            if (String.IsNullOrEmpty(macString))
+            {
+                throw new InvalidOperationException("Please fill the MAC address");
+            }
+            var mac = Regex.Replace(macString, "[^0-9A-Fa-f]", String.Empty);
+            if (mac.Length != MacAddressLengthInBytes * 2)
+            {
+                throw new InvalidOperationException($"Incorrect MAC address: {macString}, the correct format is: 01-23-45-67-89-AB or 01:23:45:67:89:AB");
+            }
+
+            var result = new byte[MacAddressLengthInBytes];
+            for (var i = 0; i < result.Length; i++)
+            {
+                var hexa = new string(new[] { mac[i * 2], mac[i * 2 + 1] });
+                result[i] = Byte.Parse(hexa, NumberStyles.HexNumber);
+            }
+            return result;
+        }
+    }
+}
---
+++ Mtf.Network/Services/WakeOnLan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Mtf.Network.Services
+{
+    public static class WakeOnLan
+    {
+        public static void SendMagicPacket(string macAddress, ushort port = 7)
+        {
+            if (String.IsNullOrWhiteSpace(macAddress))
+            {
+                return;
+            }
+            var endPoint = new IPEndPoint(IPAddress.Broadcast, port);
+            using (var clientSocket = new Socket(endPoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp))
+            {
+                clientSocket.Connect(endPoint);
+                var macByteArray = MacAddressConverter.StringToByteArray(macAddress);
+                var magicPacket = MagicPacketForger.Forge(macByteArray);
+                clientSocket.Send(magicPacket, 0, magicPacket.Length, SocketFlags.None);
+            }
+        }
+    }
+}
